5cc1fc4330309cd9d1ae926798320ca11872c116
Boyko Dmytro <dmrboyko@gmail.com>
2023-03-27 23:30:24 +0300

    added slider

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const items = document.querySelectorAll('.slider .slider-line .item');
+const sliderLine = document.querySelector('.slider .slider-line');
+let count = 0;
+let width;
+
+function init() {
+    console.log('resize');
+    width = document.querySelector('.item').offsetWidth;
+    sliderLine.style.width = width * items.length + 'px';
+    items.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.slider-next').addEventListener('click', function () {
+    count++;
+    if (count >= items.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = items.length - 1;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
