016a7a6ff38260c1328b49b24afb66475aa8cf9e
Boyko Dmytro <dmrboyko@gmail.com>
2023-04-11 13:02:38 +0300

    debugging

--- swiper/index.js
+++ swiper/index.js
@@ -2,10 +2,9 @@
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
-    speed: 500,
+    speed: 800,
     effect: 'slider',
-    slidesPerView: 1,
-    centeredSlides: true,
+    spaceBetween: 20,
 
     pagination: {
         el: '.swiper-pagination',
@@ -13,11 +12,23 @@ const swiper = new Swiper('.swiper', {
     },
 
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.showcaseSlider-next',
+        prevEl: '.showcaseSlider-prev',
     },
 
     autoplay: {
-        delay: 2000,
+        delay: 3000,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 2.4,
+        }
     },
 });
